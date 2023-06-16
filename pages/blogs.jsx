3d1173ef770a72045706ee75163a13fe1e797706
HadiScript <2019cs519@gmail.com>
2023-06-16 04:56:59 -0700

    4:55 - blogs - sat

--- pages/blogs.jsx
+++ pages/blogs.jsx
@@ -6,62 +6,64 @@ import BlogArea from "../components/blogs/BlogArea";
 import Router, { useRouter } from "next/router";
 
 const blogs = ({ blogs, recentBlogs, categories, mostView }) => {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const [allBlogs, setAllBlogs] = useState(blogs);
-  // const [RecentBlogs] = useState(recentBlogs);
-  // const [Categories] = useState(categories);
-  // const [mostViewed] = useState(mostView);
+  const [allBlogs, setAllBlogs] = useState(blogs);
+  const [RecentBlogs] = useState(recentBlogs);
+  const [Categories] = useState(categories);
+  const [mostViewed] = useState(mostView);
 
-  // const [total, setTotal] = useState(0);
-  // const [page, setPage] = useState(Number(router?.query?.page) || 1);
-  // const [loading, setLoading] = useState(false);
-  // const [searchQuery, setSearchQuery] = useState("");
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(Number(router?.query?.page) || 1);
+  const [loading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  // useEffect(() => {
-  //   if (page === 1) return;
-  //   loadMore();
-  // }, [page]);
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
 
-  // useEffect(() => {
-  //   getTotal();
-  // }, []);
+  useEffect(() => {
+    getTotal();
+  }, []);
 
-  // const loadMore = async () => {
-  //   try {
-  //     setLoading(true);
-  //     console.log("running searching2");
-  //     const { data } = await axios.get(`/blogs/${page}?search=${searchQuery}`);
-  //     if (searchQuery) {
-  //       setAllBlogs([...data.blogs, ...allBlogs]);
-  //     } else {
-  //       setAllBlogs([...allBlogs, ...data.blogs]);
-  //     }
-  //     setLoading(false);
-  //   } catch (err) {
-  //     setLoading(false);
-  //     console.log(err);
-  //   }
-  // };
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      console.log("running searching2");
+      const { data } = await axios.get(
+        `http://localhost:5000/api/blogs/${page}?search=${searchQuery}`
+      );
+      if (searchQuery) {
+        setAllBlogs([...data.blogs, ...allBlogs]);
+      } else {
+        setAllBlogs([...allBlogs, ...data.blogs]);
+      }
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
 
-  // const handleSearch = (event) => {
-  //   event.preventDefault();
+  const handleSearch = (event) => {
+    event.preventDefault();
 
-  //   console.log("running searching");
-  //   // Reset page number when performing a new search
-  //   setPage(1);
-  //   // Trigger loading more blogs with the updated search query
-  //   loadMore();
-  // };
+    console.log("running searching");
+    // Reset page number when performing a new search
+    setPage(1);
+    // Trigger loading more blogs with the updated search query
+    loadMore();
+  };
 
-  // const getTotal = async () => {
-  //   try {
-  //     const { data } = await axios.get("/blog-count");
-  //     setTotal(data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/api/blog-count");
+      setTotal(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -76,9 +78,7 @@ const blogs = ({ blogs, recentBlogs, categories, mostView }) => {
         }
         image={"/assets/images/bread.jpg"}
       />
-
-     <h2>Comming Soon :)</h2> 
-
+    
       {/* <BlogArea
         allBlogs={allBlogs}
         RecentBlogs={RecentBlogs}
@@ -100,17 +100,17 @@ const blogs = ({ blogs, recentBlogs, categories, mostView }) => {
   );
 };
 
-// export async function getServerSideProps() {
-//   const { data } = await axios.get(`/blogs/1`);
+export async function getServerSideProps() {
+  const { data } = await axios.get(`http://localhost:5000/api/blogs/1`);
 
-//   return {
-//     props: {
-//       blogs: data.blogs,
-//       recentBlogs: data.recentBlogs,
-//       categories: data.categories,
-//       mostView: data.mostView,
-//     },
-//   };
-// }
+  return {
+    props: {
+      blogs: data.blogs,
+      recentBlogs: data.recentBlogs,
+      categories: data.categories,
+      mostView: data.mostView,
+    },
+  };
+}
 
 export default blogs;
