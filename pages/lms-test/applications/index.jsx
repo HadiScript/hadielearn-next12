fc7fcd594d7e43475c20939cb24a7f028ee83f2b
HadiScript <2019cs519@gmail.com>
2023-07-18 23:06:06 -0700

    select 11 06

--- pages/lms-test/applications/index.jsx
+++ pages/lms-test/applications/index.jsx
@@ -102,6 +102,7 @@ const Applications = () => {
             }}
             id="enrollToSelect"
           >
+            <option>select</option>
             <option value="workshop">Workshop</option>
             <option value="program">Program</option>
           </select>
@@ -150,22 +151,48 @@ const Applications = () => {
           bordered
           column={{ xxl: 3, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
         >
-          <Descriptions.Item label="Email">{currentObj.email}</Descriptions.Item>
-          <Descriptions.Item label="ID Card">{currentObj.idCard}</Descriptions.Item>
-          <Descriptions.Item label="Date of birth">{currentObj.dateOfBirth}</Descriptions.Item>
-          <Descriptions.Item label="Phone Number">{currentObj.phoneNumber}</Descriptions.Item>
-          <Descriptions.Item label="WhatsApp Number">{currentObj.whatsAppphoneNumber}</Descriptions.Item>
-        
-          <Descriptions.Item label="Parent name">{currentObj.parentName}</Descriptions.Item>
-          <Descriptions.Item label="Parent Occupation">{currentObj.parentOccupations}</Descriptions.Item>
-          <Descriptions.Item label="Parent Phone">{currentObj.parentPhoneNumber}</Descriptions.Item>
+          <Descriptions.Item label="Email">
+            {currentObj.email}
+          </Descriptions.Item>
+          <Descriptions.Item label="ID Card">
+            {currentObj.idCard}
+          </Descriptions.Item>
+          <Descriptions.Item label="Date of birth">
+            {currentObj.dateOfBirth}
+          </Descriptions.Item>
+          <Descriptions.Item label="Phone Number">
+            {currentObj.phoneNumber}
+          </Descriptions.Item>
+          <Descriptions.Item label="WhatsApp Number">
+            {currentObj.whatsAppphoneNumber}
+          </Descriptions.Item>
+
+          <Descriptions.Item label="Parent name">
+            {currentObj.parentName}
+          </Descriptions.Item>
+          <Descriptions.Item label="Parent Occupation">
+            {currentObj.parentOccupations}
+          </Descriptions.Item>
+          <Descriptions.Item label="Parent Phone">
+            {currentObj.parentPhoneNumber}
+          </Descriptions.Item>
           {/* <br /> */}
-          <Descriptions.Item label="Interest">{currentObj.interest}</Descriptions.Item>
-          <Descriptions.Item label="Want to achieve">{currentObj.wantToAchieve}</Descriptions.Item>
-          <Descriptions.Item label="Enroll Into">{currentObj.enrollTo === 'program' ? currentObj?.course  :currentObj.workshop}</Descriptions.Item>
+          <Descriptions.Item label="Interest">
+            {currentObj.interest}
+          </Descriptions.Item>
+          <Descriptions.Item label="Want to achieve">
+            {currentObj.wantToAchieve}
+          </Descriptions.Item>
+          <Descriptions.Item label="Enroll Into">
+            {currentObj.enrollTo === "program"
+              ? currentObj?.course
+              : currentObj.workshop}
+          </Descriptions.Item>
           <br />
           <Descriptions.Item label="City">{currentObj.city}</Descriptions.Item>
-          <Descriptions.Item label="Address">{currentObj.address}</Descriptions.Item>
+          <Descriptions.Item label="Address">
+            {currentObj.address}
+          </Descriptions.Item>
         </Descriptions>
       </Modal>
     </>
