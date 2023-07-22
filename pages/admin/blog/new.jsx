56db2d7fc6eca10d8d5c45234e70deebe574f553
HadiScript <2019cs519@gmail.com>
2023-07-21 23:56:35 -0700

    debuged2

--- pages/admin/blog/new.jsx
+++ pages/admin/blog/new.jsx
@@ -25,23 +25,23 @@ const initialState = {
   description: "",
 };
 
-const toolbarOptions = [
-  ["bold", "italic", "underline", "strike"], // toggled buttons
-  ["blockquote", "code-block"],
-  [{ header: 1 }, { header: 2 }], // custom button values
-  [{ list: "ordered" }, { list: "bullet" }],
-  [{ script: "sub" }, { script: "super" }], // superscript/subscript
-  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-  [{ direction: "rtl" }], // text direction
-  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-  [{ font: [] }],
-  [{ align: [] }],
-  ["link"],
-  ["video"], // add video option
-  ["clean"],
-];
+// const toolbarOptions = [
+//   ["bold", "italic", "underline", "strike"], // toggled buttons
+//   ["blockquote", "code-block"],
+//   [{ header: 1 }, { header: 2 }], // custom button values
+//   [{ list: "ordered" }, { list: "bullet" }],
+//   [{ script: "sub" }, { script: "super" }], // superscript/subscript
+//   [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+//   [{ direction: "rtl" }], // text direction
+//   [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+//   [{ header: [1, 2, 3, 4, 5, 6, false] }],
+//   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+//   [{ font: [] }],
+//   [{ align: [] }],
+//   ["link"],
+//   ["video"], // add video option
+//   ["clean"],
+// ];
 
 const NewBlogAdd = () => {
   const Editor = useMemo(
@@ -304,7 +304,7 @@ const NewBlogAdd = () => {
           />
           <br />
           <Editor
-            modules={{ toolbar: toolbarOptions }}
+            // modules={{ toolbar: toolbarOptions }}
             theme="snow"
             value={content}
             onChange={setContent}
