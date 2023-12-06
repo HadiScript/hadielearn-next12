bb0e4e70747053b00d96887896b515c45f7e8f18
HadiScript <2019cs519@gmail.com>
2023-12-06 17:29:45 +0500

    checks

--- config/API.js
+++ config/API.js
@@ -1,3 +1,3 @@
 export const API = "https://api.hadielearning.com/api";
-// export const API = "http://localhost:5000/api";
+// export const API = "http://localhost:5a000/api";
 export const PPI = "https://api.hadielearning.com/api";
